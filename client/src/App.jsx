8b8d447234393f3a845c4f363e9269c706f24b9a
srinjoy-26 <srinjoydev26@gmail.com>
2024-10-14 21:20:56 +0530

    Initialized the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,13 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './page/Home';
 import All_Crop_dist from './page/All_Crop_dist';
+import Map from './components/Map';
 
 const App = () => {
   return (
-    <div>
-      {/* <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/crop' element={<All_Crop_dist/>}/>
-      </Routes>    
-      </BrowserRouter> */}
-
-      <All_Crop_dist/>
-    </div>
+    <>
+     <Map/>
+    </>
   )
 }
 
